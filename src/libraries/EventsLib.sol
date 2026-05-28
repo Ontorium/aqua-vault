9d3fcdb789d621c8d26cddabd0b38239e3f4f579
sujine2 <hwa.sujin1219@gmail.com>
2026-05-28 16:54:56 +0900

    feature: delete withdraw cancle

--- src/libraries/EventsLib.sol
+++ src/libraries/EventsLib.sol
@@ -23,7 +23,6 @@ library EventsLib {
     /// `onBehalf` address; multiple queued requests for the same user merge into one slot.
     event WithdrawalRequested(address indexed sender, address indexed onBehalf, uint256 assets, uint256 shares);
     event WithdrawalClaimed(address indexed onBehalf, uint256 assets);
-    event WithdrawalCancelled(address indexed onBehalf, uint256 shares, uint256 assets);
 
     // Vault creation events
     event Constructor(address indexed owner, address indexed asset);
